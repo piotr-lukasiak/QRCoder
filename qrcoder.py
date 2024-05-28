989af81c07956e852e22c5f873a5e30d45e0ddec
Piotr Lukasiak <piotr.lukasiak@primeline.ie>
2024-05-28 09:42:00 +0100

    First proper commit - generates sets of QR codes in A4 pages

--- qrcoder.py
+++ qrcoder.py
@@ -1,19 +1,46 @@
 import segno
+from PIL import Image, ImageDraw
 from sys import argv
 
-
+qrcodefiles = {}
 
 for argument in argv[1:]:
     qrcounter = 0
     file = open(argument,"r")
     filename = file.name[0:-4]
-    qrcodes = file.readlines()
-    qrcodes_merged = file.read()
+    qrcodefiles[filename] = [str(x).strip().split(",") for x in file.readlines()]
     file.close()
-    for code in qrcodes:
-        qrcode_IMG = segno.make(code[0:-1], error='H')
-        qrcode_IMG.save(code[0:-1] + ".png",scale=4, border= 10)
-        qrcounter += 1
+
+
+
+for qrcodefile in qrcodefiles:
+    imagecoordinates = [0,0]
+    pageno = 0
+    qrcodecodegroupimage = None 
+    #draw = ImageDraw.Draw(qrcodecodegroupimage)
+    for qrcode in qrcodefiles[qrcodefile]:
+        qrcode_IMG = segno.make_qr(qrcode[0], error='H').to_pil(scale = 15, border = 15)
+        qrcode_IMG = qrcode_IMG.resize([825,825], resample = Image.NEAREST)
+        print(imagecoordinates)
+        if qrcodecodegroupimage == None:
+            qrcodecount = len(qrcodefiles[qrcodefile])
+            qrcodecodegroupimage = Image.new("RGBA", size = [2480, 3508] )
+            pageno += 1
+        if (imagecoordinates[1]+1)*qrcode_IMG.width > 2480:
+            imagecoordinates[0] += 1
+            imagecoordinates[1] = 0 
+        if (imagecoordinates[0]+1)*qrcode_IMG.height > 3508:
+            imagecoordinates[0] = 0
+            imagecoordinates[1] = 0
+            qrcodecodegroupimage.save(qrcodefile +" "+str(pageno) + ".png") 
+            qrcodecodegroupimage = Image.new("RGBA", size = [2480, 3508] )
+            pageno += 1
+        qrcodecodegroupimage.paste(qrcode_IMG, [imagecoordinates[1]*qrcode_IMG.width,imagecoordinates[0]*qrcode_IMG.height])
+        imagecoordinates[1] += 1
+        
+
+    
+    qrcodecodegroupimage.save(qrcodefile +" "+str(pageno) + ".png")
 
 
 
